0b7899f799eab72d6f702ed59d6679df62a21f99
Frank Caron <frank@frankcaron.com>
2020-11-10 12:46:13 -0500

    Added multiple attributes

--- .sfdx/typings/lwc/apex/LightningForgotPasswordController.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "@salesforce/apex/LightningForgotPasswordController.forgotPassword" {
-  export default function forgotPassword(param: {username: any, checkEmailUrl: any}): Promise<any>;
-}
-declare module "@salesforce/apex/LightningForgotPasswordController.setExperienceId" {
-  export default function setExperienceId(param: {expId: any}): Promise<any>;
-}
